b089bf39f3f4387fbfb5d68efa9c93a7e6bcc988
Kimani Njoroge <kimani064@gmail.com>
2019-03-06 08:20:18 +0300

    create transaction model

---
+++ app/src/main/java/com/example/transact/models/Transact.kt
@@ -0,0 +1,9 @@
+package com.example.transact.models
+
+class Transact {
+    var id: Int = 0
+    var category: String = ""
+    var status: String = ""
+    var amount: String = ""
+    var info: String = ""
+}
